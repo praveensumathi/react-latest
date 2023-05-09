3dfd92e7bc22e93c04d567e8124bceb81b24ee90
praveensumathi <praveensumathi98@gmail.com>
2023-05-09 20:49:08 +0530

    added destructuring

--- src/BComponent.jsx
+++ src/BComponent.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
 function BComponent(props) {
+  const { BComponentValue, onChangeFromB } = props;
+
   return (
     <div>
       <input
         type="text"
         name="B"
         id="bComponent"
-        onChange={(e) => props.onChangeFromB(e.target.value)}
+        onChange={(e) => onChangeFromB(e.target.value)}
       />
       <h4>BComponent:</h4>
       <h6>Value From A Component:</h6>
       <p style={{ backgroundColor: "black", color: "white" }}>
-        {props.BComponentValue}
+        {BComponentValue}
       </p>
     </div>
   );
